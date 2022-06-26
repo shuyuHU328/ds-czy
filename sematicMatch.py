55ed2d8efbed69c07d2d49db856a042af6d817f3
Hugo <1621740564@qq.com>
2022-06-26 09:48:00 +0800

    0.2

--- sematicMatch.py
+++
@@ -1,4 +0,0 @@
-from gensim.models import KeyedVectors
-
-if __name__ == '__main__':
-    print()
---
+++ txt2binary.py
@@ -0,0 +1,27 @@
+from gensim.models import KeyedVectors
+from tqdm import tqdm
+
+
+def is_num(x):
+    try:
+        float(x)
+    except:
+        return False
+    return True
+
+
+def refine_embedding(inpath='tencent-ailab-embedding-zh-d100-v0.2.0-s/tencent-ailab-embedding-zh-d100-v0.2.0-s.txt',
+                     outpath='refine.txt'):
+    with open(outpath, 'w',encoding='utf8') as fout:
+        with open(inpath, 'r', encoding='utf8') as fin:
+            fout.write('1956951\t100\n')  # 先wc -l统计过滤之后的行数
+            for index, line in tqdm(enumerate(fin)):
+                if index == 0 or is_num(line.split(' ')[0]):
+                    continue
+                fout.write(line)
+
+
+if __name__ == '__main__':
+    refine_embedding()
+    wv_from_text = KeyedVectors.load_word2vec_format('refine.txt', binary=False)
+    wv_from_text.save_word2vec_format('TC_Model.bin', binary=True)
