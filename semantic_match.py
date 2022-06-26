4c8317f2fd7d1476d252128bfb52878ddec46795
Hugo <1621740564@qq.com>
2022-06-26 16:09:32 +0800

    version 0.3

--- semantic_match.py
+++ semantic_match.py
@@ -1,4 +1,18 @@
 from gensim.models import KeyedVectors
+import jieba
+
+
+def get_dict(dict, target_str, model):
+    leg = jieba.lcut(target_str)
+    vec = model.get_mean_vector(leg)
+    dict[target_str] = vec
+
 
 if __name__ == '__main__':
-    wv_from_text=KeyedVectors.load_word2vec_format('TC_Model.bin',binary=True)
+    model = KeyedVectors.load('test.bin')
+    file = open('standardMeta.txt', 'r', encoding='utf-8')
+    myDict = {}
+    for line in file:
+        get_dict(myDict, line.strip(), model)
+    print(len(myDict))
+    # print(model.n_similarity(leg1, leg2))
